89365c4377b394fadaf3e8dd2932dcbf24350920
a.shilo <a.shilo@baum.ru>
2025-04-01 10:12:43 +0300

    123

---
+++ task18_02_2025_task1/task18_02_2025_task1/task18_02_2025_task1.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <algorithm> // Для std::sort
+#include <random>
+using namespace std;
+int randomNumber(int min, int max) {
+    // Генерация случайного числа типа int в заданном диапазоне
+    static random_device rd; // Получаем случайное число из устройства
+    static mt19937 gen(rd()); // Инициализация генератора
+    uniform_int_distribution<int> dis(min, max); // Диапазон для int
+    return dis(gen);
+}
+void initarray(const int n, int* arr) {   
+        for (int x = 0; x < n; x++) {
+            arr[x] = randomNumber(1, 100);//y * n + x; // Инициализация массива
+        }   
+}
+void printArray(const int n, int* arr) {  
+        for (int x = 0; x < n; x++) {
+            cout << arr[x] << " ";
+            cout << endl;
+        }          
+}
+void freearray(int* array, const int n) {       
+    delete[] array; // Освобождаем массив указателей
+}
+// Проверка на простое число
+bool isPrime(const int num) {
+    if (num <= 1) return false;
+    if (num == 2) return true;
+    if (num % 2 == 0) return false;
+    for (int i = 3; i * i <= num; i += 2)
+        if (num % i == 0) return false;
+    return true;
+}
+
+void printi_sPrime(const int n, int* arr) {    
+    int count = 0;
+        for (int i = 0; i < n; i++) {            
+            if (isPrime(arr[i])) {
+               
+                cout << "простые: " << arr[i] << '\n';
+                count++;                
+            }    
+        }
+        cout << "сколько раз встречаются простые" << count << endl;
+}
+int count_not_print(const int n, int* arr) {
+    int cant = 0;
+    for (int i = 0; i < n; i++) {
+        if (!isPrime(arr[i])) {
+            cant++;
+        }
+    }    
+    return cant;
+}
+// Удаление простых чисел + уменьшение размера
+void removePrimes(int*& arr, int& size) {
+    // 1. Считаем, сколько чисел останется
+    int  notprime;
+    notprime = count_not_print(size, arr);
+
+    // 2. Создаём новый массив    
+
+    int* newArr = new int [notprime];
+
+        //копируем только не простые числа в новый массив
+  
+        int j = 0;
+    for (int i = 0; i < size; i++) {
+        if (!isPrime(arr[i])) {
+            newArr[j] = arr[i];
+            j++;
+        }
+    }
+
+
+   // 3. Удаляем старый, заменя новым
+    delete[] arr;
+    arr = newArr;
+    size = notprime;
+
+}
+int main() {   
+    setlocale(LC_ALL, "");
+    int n = 10; // Размерность матрицы  
+    int* intArray = new int[n];     
+    initarray(n, intArray);
+    printArray(n, intArray);
+    removePrimes(intArray, n);
+    cout << "после\n";
+    printArray(n, intArray);
+
+    //printi_sPrime(n, intArray);
+    //int notprime = 0;
+    //notprime =count_not_print(n, intArray);
+    //cout << "сколько раз встречаются не простые" << notprime << endl;
+    freearray(intArray, n);
+    return 0; 
+}
---
+++ task18_02_2025_task2/task18_02_2025_task2/task18_02_2025_task2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+using namespace std;
+void sortElements(int* arr, int size) {
+
+    int* posArr = new int[size];
+    
+    int* negArr = new int[size];
+    
+    int* zeroArr = new int[size];
+    
+    int posIndex = 0;
+    
+    int negIndex = 0;
+    
+    int zeroIndex = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > 0) {
+            posArr[posIndex] = arr[i];
+            posIndex++;
+        }
+
+        else if (arr[i] < 0) {
+
+            negArr[negIndex] = arr[i];
+            negIndex++;
+
+        }
+
+        else {
+            zeroArr[zeroIndex] = arr[i];
+            zeroIndex++;
+        }
+    }
+    cout << "обычные: ";
+
+    for (int i = 0; i < posIndex; i++) {
+
+        cout << posArr[i] << " ";
+
+    }
+
+    cout << endl;
+
+    cout << "отрицательные: ";
+
+    for (int i = 0; i < negIndex; i++) {
+
+        cout << negArr[i] << " ";
+
+    }
+
+    cout << endl;
+
+    cout << "нулевые: ";
+
+    for (int i = 0; i < zeroIndex; i++) {
+
+        cout << zeroArr[i] << " ";
+
+    }
+
+    cout << endl;
+
+    delete[] posArr;
+
+    delete[] negArr;
+
+    delete[] zeroArr;
+
+}
+int main() {
+    setlocale(LC_ALL, "");
+    int arr[] = { 2, -4, 5, -7, 9, -10 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sortElements(arr, size);
+    return 0;
+}
