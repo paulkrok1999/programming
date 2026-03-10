8e5ca24525b7a184e1db3d85c4b898fe1eb60ed6
paulkrok1999 <paulshilo1999@gmail.com>
2026-03-10 16:32:36 +0300

    123

---
+++ shilo_06_03_2026_2/shilo_06_03_2026_2/Program.cs
@@ -0,0 +1,58 @@
+﻿class TestClass {
+    static void Main() {
+        Console.Write("Чтобы начать играть, выбери уровень:" +
+            "\n1. легко (от 0 до 20)" +
+            "\n2. средний (от 0 до 50)" +
+            "\n3. сложный (от 0 до 100)" +
+            "\nТвой выбор: ");
+
+        string choice = Console.ReadLine();
+        int MaxNum = 0;
+
+        switch (choice)
+        {
+            case "1":
+                MaxNum = 20;
+                break;
+            case "2":
+                MaxNum = 50;
+                break;
+            case "3":
+                MaxNum = 100;
+                break;
+            default:
+                Console.WriteLine("Неверный выбор. Играем в легкий уровень.");
+                MaxNum = 20;
+                break;
+        }
+
+        Random random = new();
+        int secret = random.Next(0, MaxNum + 1); // +1 чтобы включить maxNumber
+        int pop = 0;
+        bool isGuessed = false;
+
+        Console.WriteLine($"\nЯ загадал число от 0 до {MaxNum}. Попробуй угадать!");
+
+        while (!isGuessed) {
+            Console.Write("Введи число: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int guess)) {
+                pop++;
+                if (guess == secret) {
+                    Console.WriteLine($"Поздравляю! Ты угадал число {secret} за {pop} попыток!");
+                    isGuessed = true;
+                }
+                else if (guess < secret) {
+                    Console.WriteLine("Загаданное число БОЛЬШЕ. Попробуй еще!");
+                }
+                else {
+                    Console.WriteLine("Загаданное число МЕНЬШЕ. Попробуй еще!");
+                }
+            }
+            else {
+                Console.WriteLine("Пожалуйста, введи корректное число!");
+            }
+        }
+    }
+}
